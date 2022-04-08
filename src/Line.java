5b8e7ad09706c54c44c6d68f7738e1ae20d1aaf8
BT_1E10_20 <sample@bths.edu>
2022-04-08 09:44:39 -0400

    Added a little more code.

--- src/Line.java
+++ src/Line.java
@@ -1,2 +1,16 @@
-public class Line {
+import java.awt.Graphics;
+
+public class Line extends Shape
+{
+    public Line()
+    {
+        super();
+    }
+
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(getColor());
+
+    }
 }
